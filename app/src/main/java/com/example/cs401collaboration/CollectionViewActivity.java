067063a81b030ee7ee212e7c0749f4309d116025
bschooling <bryceschooling@gmail.com>
2022-04-13 13:21:40 -0700

    Home Screen Mockup Collection View Mockup

---
+++ app/src/main/java/com/example/cs401collaboration/CollectionViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.cs401collaboration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CollectionViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection_view);
+    }
+}
