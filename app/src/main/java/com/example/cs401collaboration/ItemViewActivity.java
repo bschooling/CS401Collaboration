c32d19583e2b563ede45c880148f501cabcdc84d
bschooling <bryceschooling@gmail.com>
2022-04-13 14:50:52 -0700

    Add Item View Screen Mockup Add Collection View Screen Mockup

---
+++ app/src/main/java/com/example/cs401collaboration/ItemViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.cs401collaboration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_view);
+    }
+}
