e6d9afd1ea289e0b104262ef4f1d882de1ef8794
Arshdeep Padda <aspadda@icloud.com>
2022-04-15 00:08:39 -0700

    Added Login / Signup Functionality
    Added LoginActivity and Login / Signup Functionality.

--- app/src/main/java/com/example/cs401collaboration/HomeScreenActivity.java
+++ app/src/main/java/com/example/cs401collaboration/HomeScreenActivity.java
@@ -2,13 +2,54 @@ package com.example.cs401collaboration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
-public class HomeScreenActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeScreenActivity extends AppCompatActivity
+{
+
+    /* Firebase Auth */
+    private FirebaseAuth mAuth;
+
+    private String LOG_TAG_MAIN = "HomeScreenActivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        // Check if user is logged in. Direct to login screen if not.
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        FirebaseAuth.getInstance().signOut();
+
+        if(currentUser == null)
+        {
+            Log.d(LOG_TAG_MAIN, "onCreate: User not logged in");
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+        else Log.d(LOG_TAG_MAIN, "onCreate: User logged in");
     }
+
+    @Override
+    protected void onStart ()
+    {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() == null) return;
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null)
+            Log.d(LOG_TAG_MAIN, "onStart: User logged in");
+        else
+            Log.d(LOG_TAG_MAIN, "onStart: User not logged in");
+    }
+
 }
---
+++ app/src/main/java/com/example/cs401collaboration/LoginActivity.java
@@ -0,0 +1,172 @@
+package com.example.cs401collaboration;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity
+{
+    /* Firebase Auth */
+    private FirebaseAuth mAuth;
+
+    /* Handlers for UI elements */
+    Button btLogin;
+    Button btSignup;
+    EditText etEmail;
+    EditText etPassword;
+
+    /* Log tag */
+    private final String LOG_TAG_MAIN = "LoginActivity";
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        /* Handle UI elements */
+        btLogin = (Button) findViewById(R.id.bt_loginact_login);
+        btSignup = (Button) findViewById(R.id.bt_loginact_signup);
+        etEmail = (EditText) findViewById(R.id.et_loginact_email);
+        etPassword = (EditText) findViewById(R.id.etp_loginact_password);
+
+        /* Attach button OnClickListeners */
+        btLogin.setOnClickListener(new btLoginOnClickListener());
+        btSignup.setOnClickListener(new btSignupOnClickListener());
+    }
+
+    /**
+     * Handles onClick of Login button.
+     *
+     * Check if fields are null, try to login, and either toast failure
+     * or return to calling activity.
+     *
+     * @author Arshdeep Padda
+     */
+    private class btLoginOnClickListener implements View.OnClickListener
+    {
+        public void onClick (View v)
+        {
+            // Get fields
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+
+            // Check if null, abort if so.
+            if (email.isEmpty() || password.isEmpty())
+            {
+                Toast.makeText (
+                        LoginActivity.this,
+                        "Fields cannot be empty.",
+                        Toast.LENGTH_LONG
+                ).show();
+                return;
+            }
+
+            // Attempt login
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            // Success: return to calling activity
+                            Log.d(LOG_TAG_MAIN, "signInWithEmail:success");
+                            finish();
+                        }
+                        else
+                        {
+                            // Failure: report
+                            Log.w(LOG_TAG_MAIN, "signInWithEmail:failure", task.getException());
+                            Toast.makeText (
+                                    LoginActivity.this,
+                                    "Could not log you in",
+                                    Toast.LENGTH_LONG
+                            ).show();
+                        }
+                    }
+                });
+        }
+    }
+
+    /**
+     * Handles onClick of Signup button.
+     *
+     * Check if fields are null, password length is good, tries to create user,
+     * and either toast failure or return to calling activity upon successful creation
+     * and login of new user.
+     *
+     * @author Arshdeep Padda
+     */
+    private class btSignupOnClickListener implements View.OnClickListener
+    {
+        public void onClick (View v)
+        {
+            // Get email and password
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+
+            // Ensure neither field is empty, and password of acceptable length.
+            // Abort otherwise.
+            if (email.isEmpty() || password.isEmpty())
+            {
+                Toast.makeText (
+                        LoginActivity.this,
+                        "Fields cannot be empty.",
+                        Toast.LENGTH_LONG
+                ).show();
+                return;
+            }
+            if (password.length() < 7)
+            {
+                Toast.makeText (
+                        LoginActivity.this,
+                        "Password must be at least 6 characters long.",
+                        Toast.LENGTH_LONG
+                ).show();
+                return;
+            }
+
+            // Attempt Creation
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful())
+                            {
+                                // Success: return to calling activity with user logged in
+                                Log.d(LOG_TAG_MAIN, "createUserWithEmail:success");
+                                finish();
+                            }
+                            else
+                            {
+                                // Failure: report
+                                Log.d(LOG_TAG_MAIN, "createUserWithEmail:failure", task.getException());
+                                Log.d(LOG_TAG_MAIN, task.getException().getLocalizedMessage());
+                                Toast.makeText(
+                                        LoginActivity.this,
+                                        "Unable to authenticate.",
+                                        Toast.LENGTH_LONG
+                                ).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+}
