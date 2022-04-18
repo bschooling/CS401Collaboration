788de4c2eeb20763cd2f2319ab55174d2b7bd18e
bschooling <bryceschooling@gmail.com>
2022-04-18 15:39:57 -0700

    Add clickable tiles Add transition to new Activity

--- app/src/main/java/com/example/cs401collaboration/CollectionViewActivity.java
+++ app/src/main/java/com/example/cs401collaboration/CollectionViewActivity.java
@@ -2,7 +2,13 @@ package com.example.cs401collaboration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.cs401collaboration.model.Collection;
 
 public class CollectionViewActivity extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class CollectionViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collection_view);
+        Intent intent = getIntent();
+
+        TextView mCollectionLocation = findViewById(R.id.collection_view_location);
+        TextView mCollectionDescription = findViewById(R.id.collection_view_description);
+        Toolbar mCollectionBar = findViewById(R.id.collectionToolbar);
+        ImageView mCollectionImage = findViewById(R.id.collection_app_bar_image);
+
+        mCollectionLocation.setText(intent.getStringExtra("collection_clicked_location"));
+        mCollectionDescription.setText(intent.getStringExtra("collection_clicked_description"));
+        mCollectionBar.setTitle(intent.getStringExtra("collection_clicked_title"));
+        mCollectionImage.setImageResource(intent.getIntExtra("collection_clicked_image", 0));
+
+        //Id is passed in, and collected here.  No functionality is used yet
+        int collectionID = intent.getIntExtra("collection_clicked_id", 0);
+
     }
 }
--- app/src/main/java/com/example/cs401collaboration/rvAdapters/CollectionRvAdapter.java
+++ app/src/main/java/com/example/cs401collaboration/rvAdapters/CollectionRvAdapter.java
@@ -1,6 +1,7 @@
 package com.example.cs401collaboration.rvAdapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.cs401collaboration.CollectionViewActivity;
 import com.example.cs401collaboration.R;
 import com.example.cs401collaboration.model.Collection;
 
@@ -43,7 +45,23 @@ public class CollectionRvAdapter extends RecyclerView.Adapter<CollectionRvAdapte
         Collection collection = collectionArrayList.get(position);
         holder.collectionName.setText(collection.getName());
         holder.collectionLocation.setText(collection.getLocation());
-        //TODO override image
+        //TODO swap hardcoded image for image from collection
+        holder.collectionImage.setImageResource(android.R.drawable.ic_menu_gallery);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*  Creates new activity for the collection */
+                Intent intent = new Intent(context, CollectionViewActivity.class);
+                intent.putExtra("collection_clicked_id", collection.getDocID());
+                intent.putExtra("collection_clicked_title", collection.getName());
+                intent.putExtra("collection_clicked_description", collection.getDescription());
+                intent.putExtra("collection_clicked_location", collection.getLocation());
+                //TODO change hardcoded image call
+                int image = android.R.drawable.ic_menu_gallery;
+                intent.putExtra("collection_clicked_image", image);
+                context.startActivity(intent);
+            }
+        });
     }
 
     /** gets number of views to create */
@@ -54,13 +72,14 @@ public class CollectionRvAdapter extends RecyclerView.Adapter<CollectionRvAdapte
     public class Viewholder extends RecyclerView.ViewHolder {
 
         private TextView collectionName, collectionLocation;
-        //TODO add image
+        private ImageView collectionImage;
+
 
         public Viewholder (@NonNull View itemView) {
             super(itemView);
             collectionName = itemView.findViewById(R.id.collection_name);
             collectionLocation = itemView.findViewById(R.id.collection_item_location);
-            //TODO add bind for image
+            collectionImage = itemView.findViewById(R.id.collection_image);
         }
     }
 
