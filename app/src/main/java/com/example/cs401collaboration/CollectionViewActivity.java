75e2a4d9dc064ce3252899ee4bf202a475dcda43
nam <npham22@horizon.csueastbay.edu>
2022-04-22 15:23:03 -0700

    Hid title bar of app in CollectionViewActivity

--- app/src/main/java/com/example/cs401collaboration/CollectionViewActivity.java
+++ app/src/main/java/com/example/cs401collaboration/CollectionViewActivity.java
@@ -39,6 +39,7 @@ public class CollectionViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_collection_view);
 
         mDB = DatabaseService.getInstance();
